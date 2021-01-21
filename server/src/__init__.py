11f7fd31741e76277c34dbc57620ca42e230c323
joshuanixon <nixonjoshua98@gmail.com>
2021-01-21 13:01:29 +0000

    OK

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -2,8 +2,6 @@ import os
 
 from flask_pymongo import PyMongo
 
-from configparser import ConfigParser
-
 from src import utils
 
 from src.staticobjects import Loot, Bounty, Weapon
@@ -12,14 +10,13 @@ from src.classes import FlaskApplication
 
 
 def create_app():
-	config = ConfigParser()
-
-	config.read("config.ini")
-
 	app = FlaskApplication(__name__)
 
 	app.mongo = PyMongo()
 
-	app.mongo.init_app(app, uri=config.get("debug" if os.getenv("DEBUG") == "1" else "production", "MONGO_URI"))
+	if (uri := os.getenv("MONGO_URI")) is None:
+		uri = "mongodb://localhost:27017/greedymercs"
+
+	app.mongo.init_app(app, uri=uri)
 
 	return app

e0ace0ef8c9bf278698c3d20eabaa0f97ab0f95e
joshuanixon <nixonjoshua98@gmail.com>
2021-01-23 18:21:46 +0000

    Updated

--- server/src/__init__.py
+++ server/src/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from pymongo import MongoClient
+from flask_pymongo import PyMongo
 
 from src import utils
 
@@ -10,11 +10,11 @@ from src.classes import FlaskApplication
 def create_app():
 	app = FlaskApplication(__name__)
 
+	app.mongo = PyMongo()
+
 	if (uri := os.getenv("MONGO_URI")) is None:
 		uri = "mongodb://localhost:27017/greedymercs"
 
-	app.mongo = MongoClient(uri)
-
-	app.mongo.db = app.mongo.greedymercs
+	app.mongo.init_app(app, uri=uri)
 
 	return app

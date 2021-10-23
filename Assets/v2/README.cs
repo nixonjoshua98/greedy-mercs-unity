09d93fff12230a945067dc081f2906afb228b248
Joshua Nixon <nixonjoshua98@gmail.com>
2021-10-23 21:06:04 +0100

    README changes

--- Assets/v2/README.cs
+++ Assets/v2/README.cs
@@ -5,12 +5,4 @@
 
     GM.*.Models
         These classes act as (server) models as well as data structures. We should be very cautious when changing or adding properties  
-
-    Extensions
-        .Format() -> My custom ToString implementations but seperated for ease of use
-    
-    Armoury Items
-        - Level -> (level * (baseEffect * (1 + rating))) * grade?
-        - Grade (B, C, D etc) cannot be changed - Gives base multiplier
-        - Star Rating (Level) (1, 2, 3, 4, 5) can be improved - Improves per level improvement
  */

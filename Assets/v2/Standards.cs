cc5eab206b749ccaf3ba67ec551c1164a2e643ca
Joshua Nixon <nixonjoshua98@gmail.com>
2022-01-29 15:37:03 +0000

    Minor

--- Assets/v2/Standards.cs
+++ Assets/v2/Standards.cs
@@ -1,5 +1,7 @@
 ﻿
 /*
+    Parallax background needs fixing
+ 
     __Model Classes__
         - Models are primarily used as data transfer objects between either the server or file system        
         - Model classes can contain other attributes which are set at runtime (ex. values from scriptable objects) but once a model 'requires' a method to be implemented,
